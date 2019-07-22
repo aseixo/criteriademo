27ebd1ac980301e41472062113ec95255c18413f
aseixo <aseixo@zoho.com>
2019-07-22 21:38:51 +0200

    cambios addCorrupto

--- src/test/java/com/iuglans/criteria/CriteriaappApplicationTests.java
+++ src/test/java/com/iuglans/criteria/CriteriaappApplicationTests.java
@@ -229,7 +229,7 @@ public class CriteriaappApplicationTests {
 		Optional<Actividade> a = actividadeRepository.findById(8L);
 		List<Actividade> acts = new ArrayList<>();
 		acts.add(a.get());
-		Corrupto c = new Corrupto(1L, "Candia", "BeBé", "suborno a deputado provincial", 0L, acts);
+		Corrupto c = new Corrupto(101L, "Candianinha", "BeBé", "suborno a deputado provincial", 0L, acts);
 		corruptos.save(c);
 		logger.info(c.toString());
 		assert(corruptos.count() > old);
